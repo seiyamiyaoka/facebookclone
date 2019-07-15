c909fe40f91713cef2a0c812493d1a4dcafc509f
yasumasa abe <abeyasumasa@MacBook-Air.local>
2019-07-15 14:10:00 +0900

    確認画面でvalidatesを表示

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -24,6 +24,7 @@ class PicturesController < ApplicationController
 
   def confirm
     @picture = Picture.new(picture_params)
+    render :new if @picture.invalid?
   end
 
   # GET /pictures/1/edit
