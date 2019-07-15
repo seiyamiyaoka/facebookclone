cc5438e0c9e17b4df1bfe7cc33ccc0acb545563b
yasumasa abe <abeyasumasa@MacBook-Air.local>
2019-07-15 18:23:32 +0900

    確認画面でユーザーの値が取得できていないため修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,7 +23,7 @@ class PicturesController < ApplicationController
   end
 
   def confirm
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
 
